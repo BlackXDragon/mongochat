b3d8fb4ac5c4b965cb081d9405db42e3ce462de4
Malolan V <BlackXDragon@users.noreply.github.com>
2019-04-11 21:52:08 +0530

    Notifs actions and merging

--- static/sw.js
+++ static/sw.js
@@ -1,14 +1,63 @@
 console.log('Service Worker Running');
 
+var notifs = [];
+
 self.addEventListener('push', e => {
     console.log('Got Push Notification');
     const data = e.data.json();
-    console.log(data);
-    self.registration.showNotification(data.name, {
-        body: data.message,
-        icon: '/msg.png'
-    });
-})
+    notifs.push(data);
+    if(notifs.length<=1) {
+        self.registration.showNotification(data.name, {
+            body: data.message,
+            icon: '/msg.png',
+            sound: '/jingle-all-the-way.wav',
+            actions: [{
+                action: 'read',
+                title: 'Mark as Read'
+            }],
+            data: {
+                url: '/'
+            }
+        });
+    } else {
+        body = '';
+        notifs.forEach((notif) => {
+            body += notif.name+': '+notif.message+'\n';
+        });
+        self.registration.showNotification('New messages', {
+            body: body,
+            icon: '/msg.png',
+            sound: '/jingle-all-the-way.wav',
+            actions: [{
+                action: 'read',
+                title: 'Mark as Read'
+            }],
+            data: {
+                url: '/'
+            }
+        });
+    }
+});
+
+self.addEventListener('notificationclick', e => {
+    if (!e.action) {
+        e.notification.close();
+        var found = false;
+        clients.matchAll().then(function (clientsArr) {
+            for (i = 0; i < clientsArr.length; i++) {
+                if (clientsArr[i].url === event.data.url) {
+                    found = true;
+                    clientsArr[i].focus();
+                    break;
+                }
+            }
+            if (!found) {
+                clients.openWindow(event.data.url).then(function (windowClient) {
+                });
+            }
+        });
+    }
+});
 
 self.addEventListener('install', event => {
     self.skipWaiting();
@@ -19,7 +68,7 @@ self.addEventListener('fetch', event => {
         caches.match(event.request)
         .then(response => response || fetch(event.request))
         .catch(() => {
-            if(event.request.mode == 'navigate') {
+            if (event.request.mode == 'navigate') {
                 return 'Sorry, not connected.'
             }
         })
